608e83f9a47961de06e00eca289ae925bd3caff9
PlatenPrime <proshta@gmail.com>
2022-08-09 08:34:53 +0300

    insomnia

---
+++ server/index.js
@@ -0,0 +1,30 @@
+import express from "express";
+
+
+const app = express();
+
+
+app.use(express.json());
+
+
+app.get('/', (req, res) => {
+	res.send('aaaa Hello World!');
+});
+
+
+app.post('/auth/login', (req, res) => {
+	console.log(req.body);
+	res.json({
+		success: true,
+	});
+});
+
+
+
+app.listen(4444, (err) => {
+	if (err) {
+		return console.log(err);
+	}
+
+	console.log('Server OK');
+});
